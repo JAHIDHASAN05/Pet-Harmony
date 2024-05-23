f985058680efeb5204ed848dac4c2a845981a102
JAHID HASAN <developerjahid@gmail.com>
2024-05-23 19:45:37 +0600

    Seach bar intial setup

--- src/app/(withMainLayOut)/page.tsx
+++ src/app/(withMainLayOut)/page.tsx
@@ -1,9 +1,11 @@
 import AvailablePets from '@/components/ui/HomePage/AvailablePets/AvailablePets';
+import SearchSystem from '@/components/ui/HomePage/SearchBar/SearchBar';
 import React from 'react';
 
 const HomePage = () => {
     return (
         <div className='min-h-screen'>
+           <SearchSystem/>
            <AvailablePets/>
         </div>
     );
