10a2ff93d25a97d1d62fd530ff45b2dd641d8737
JAHID HASAN <developerjahid@gmail.com>
2024-05-23 20:33:45 +0600

    Banner Home page intial done

---
+++ src/components/ui/HomePage/Banner/Banner.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Banner: React.FC = () => {
+  return (
+    <div className="relative h-96 md:h-[50rem] bg-cover bg-center" style={{ backgroundImage: "url('https://media.istockphoto.com/id/1433858575/photo/man-stroking-his-old-dog.jpg?s=2048x2048&w=is&k=20&c=AtGlwqO2TwAj0bwD6LcfmRcvmPWbdcBhESUIkxP1Fmg=')" }}>
+      <div className="absolute inset-0 bg-black bg-opacity-25 flex flex-col items-center justify-center">
+        <h1 className="text-white text-4xl font-bold mb-4">Got <span className="text-red-500">Love</span>? Give <span className="text-red-500">Love</span>.</h1>
+        <button className="px-6 py-3 bg-white text-black font-semibold rounded-full shadow-lg hover:bg-gray-200 transition duration-300">Donate</button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
