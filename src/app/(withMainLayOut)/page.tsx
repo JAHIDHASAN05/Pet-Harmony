9dcdc044e6e3fa67c1d6f1a438471e1f585d1065
JAHID HASAN <developerjahid@gmail.com>
2024-05-23 21:06:59 +0600

    Banner component showing only homepage not whole layout

--- src/app/(withMainLayOut)/page.tsx
+++ src/app/(withMainLayOut)/page.tsx
@@ -1,16 +1,17 @@
-import AvailablePets from '@/components/ui/HomePage/AvailablePets/AvailablePets';
-import Banner from '@/components/ui/HomePage/Banner/Banner';
-import SearchSystem from '@/components/ui/HomePage/SearchBar/SearchBar';
-import React from 'react';
+import AvailablePets from "@/components/ui/HomePage/AvailablePets/AvailablePets";
+import Banner from "@/components/ui/HomePage/Banner/Banner";
+import SearchSystem from "@/components/ui/HomePage/SearchBar/SearchBar";
+import React from "react";
 
 const HomePage = () => {
-    return (
-        <div className='min-h-screen'>
-            
-           <SearchSystem/>
-           <AvailablePets/>
-        </div>
-    );
+  return (
+    <div className="min-h-screen">
+     
+
+      <SearchSystem />
+      <AvailablePets />
+    </div>
+  );
 };
 
 export default HomePage;
--- src/components/ui/HomePage/Banner/Banner.tsx
+++ src/components/ui/HomePage/Banner/Banner.tsx
@@ -1,8 +1,15 @@
-import bannerBg from '@/assets/bannerBg.png';
+'use client'
+import { usePathname, useRouter } from 'next/navigation'; // Import from next/navigation instead of next/router
 
 const Banner = () => {
+  const router = usePathname(); 
+
+  if (!(router === '/')) {
+    return null;
+  }
+
   return (
-    <div className="relative h-96 md:h-[40rem] bg-cover bg-center" style={{ backgroundImage: `url('https://i.ibb.co/17VZ6XK/bannerBg.png')` }}>
+    <div className="relative  h-96 md:h-[40rem] bg-cover bg-center" style={{ backgroundImage: `url('https://i.ibb.co/17VZ6XK/bannerBg.png')` }}>
       <div className="absolute inset-0 bg-black bg-opacity-25 flex flex-col items-center justify-center">
         <h1 className="text-white text-4xl font-bold mb-4">
           Got <span className="text-red-500">Love</span>? Give <span className="text-red-500">Love</span>.
