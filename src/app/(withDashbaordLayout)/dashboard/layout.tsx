390927e77e05dd16aa6fd20503c8a5bffda2efce
JAHID HASAN <developerjahid@gmail.com>
2024-05-25 03:18:39 +0600

    Dashbaourd layout private route done

--- src/app/(withDashbaordLayout)/dashboard/layout.tsx
+++ src/app/(withDashbaordLayout)/dashboard/layout.tsx
@@ -1,12 +1,15 @@
-import DashboardDrawer from '@/components/Dashboard/DashboardDrawer';
-import React, { ReactNode } from 'react';
+import DashboardDrawer from "@/components/Dashboard/DashboardDrawer";
+import PrivateRoute from "@/utils/auth/PrivateRoute";
+import { isLoggedIn } from "@/utils/auth/auth.service";
+import React, { ReactNode } from "react";
 
-const layout = ({children}:{children:ReactNode}) => {
-    return (
-        <DashboardDrawer>
-            {children}
-        </DashboardDrawer>
-    );
+const layout = ({ children }: { children: ReactNode }) => {
+
+  return (
+    <PrivateRoute>
+      <DashboardDrawer>{children}</DashboardDrawer>
+    </PrivateRoute>
+  );
 };
 
 export default layout;
---
+++ src/utils/auth/PrivateRoute.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { usePathname, useRouter } from "next/navigation";
+import React, { ReactNode } from "react";
+import { isLoggedIn } from "./auth.service";
+
+const PrivateRoute = ({ children }:{children:ReactNode}) => {
+  const router = useRouter();
+
+  const pathName = usePathname();
+
+  const isLoggedInUser = isLoggedIn();
+  console.log(isLoggedInUser);
+
+  if (!isLoggedInUser) {
+    router.push("/login");
+    localStorage.setItem("redirectAfterLoginPath", pathName);
+  }
+  else{
+      return <>{children}</>;
+    }
+    
+return <div className="min-h-screen"></div>;
+};
+
+export default PrivateRoute;
