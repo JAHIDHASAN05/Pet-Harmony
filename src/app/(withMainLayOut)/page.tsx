e06ba9d2736898c84c2c08b0caf95e8daa9a90c0
oliullah05 <mdolihasan@gmail.com>
2024-05-26 19:42:10 +0600

    homePage filtering done

--- src/app/(withMainLayOut)/page.tsx
+++ src/app/(withMainLayOut)/page.tsx
@@ -1,12 +1,8 @@
 import AvailablePets from "@/components/ui/HomePage/AvailablePets/AvailablePets";
-import Banner from "@/components/ui/HomePage/Banner/Banner";
-import SearchSystem from "@/components/ui/HomePage/SearchBar/SearchBar";
-import React from "react";
 
 const HomePage = () => {
   return (
     <div className="min-h-screen">   
-      <SearchSystem />
       <AvailablePets />
     </div>
   );
