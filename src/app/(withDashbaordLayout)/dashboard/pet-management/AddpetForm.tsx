6544f783996352831d71c87482aad00a95bfd5c1
oliullah05 <mdolihasan@gmail.com>
2024-06-05 16:23:50 +0600

    banner photo upload from frontend done

--- src/app/(withDashbaordLayout)/dashboard/pet-management/AddpetForm.tsx
+++ src/app/(withDashbaordLayout)/dashboard/pet-management/AddpetForm.tsx
@@ -5,13 +5,6 @@ import Image from "next/image";
 import React, { useState, ChangeEvent, FormEvent } from "react";
 import { toast } from "sonner";
 
-// interface FormData {
-//   temperament: string;
-//   medicalHistory: string;
-//   adoptionRequirements: string;
-
-//   // Health status (vaccinated, spayed/neutered)
-// }
 interface FormData {
   id: string;
   name: string;
@@ -30,26 +23,7 @@ interface FormData {
   medicalHistory: string;
   adoptionRequirements: string;
 }
-// interface IPetCreateData {
-//   id: string;
-//   name: string;
-//   species: string;
-//   bannerPhoto: string;
-//   multiplePhotos: string[];
-//   breed: string;
-//   age: number;
-//   specialNeeds: string;
-//   size: string;
-//   gender: string;
-//   location: string;
-//   healthStatus: string;
-//   description: string;
-//   temperament: string;
-//   medicalHistory: string;
-//   adoptionRequirements: string;
-//   createdAt: string;
-//   updatedAt: string;
-// }
+
 
 
 interface FormData {
@@ -59,7 +33,7 @@ interface FormData {
   bannerPhoto: string;
   multiplePhotos: string[];
   breed: string;
-  age: number; 
+  age: number;
   specialNeeds: string;
   size: string;
   gender: string;
@@ -72,35 +46,35 @@ interface FormData {
 }
 
 
-const AddPetForm: React.FC = ({isPetDelete, setIsPetDelete}) => {
+const AddPetForm = ({ isPetDelete, setIsPetDelete }: { isPetDelete: any, setIsPetDelete: any }) => {
   const [multiplePhotos, setMultiplePhotos] = useState<File[]>([]);
-  const [bannerPhoto, setBannerPhoto] = useState<File[]>([]);
+  const [bannerPhoto, setBannerPhoto] = useState("");
+
+  const image_hosting_url = `https://api.imgbb.com/1/upload?key=${process.env.NEXT_PUBLIC_IMGBB_API_KEY}`
+  const initialFormData: FormData = {
+    name: "",
+    species: "",
+    bannerPhoto: "",
+    multiplePhotos: [],
+    breed: "",
+    age: 0,
+    specialNeeds: "",
+    size: "",
+    gender: "",
+    location: "",
+    healthStatus: "",
+    description: "",
+    temperament: "",
+    medicalHistory: "",
+    adoptionRequirements: ""
+  };
+
+  const [formData, setFormData] = useState<FormData>(initialFormData);
 
 
-const initialFormData: FormData = {
-  name: "",
-  species: "",
-  bannerPhoto: "",
-  multiplePhotos: [],
-  breed: "",
-  age:0,
-  specialNeeds: "",
-  size: "",
-  gender: "",
-  location: "",
-  healthStatus: "",
-  description: "",
-  temperament: "",
-  medicalHistory: "",
-  adoptionRequirements: ""
-};
-
-const [formData, setFormData] = useState<FormData>(initialFormData);
-
-
-  const handleInputChange = (    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>  ) => {
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
     console.log(e);
-    
+
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
@@ -111,51 +85,63 @@ const [formData, setFormData] = useState<FormData>(initialFormData);
       setMultiplePhotos([...multiplePhotos, ...selectedPhotos]);
     }
   };
-  const handleSinglePhotoChange = (e: ChangeEvent<HTMLInputElement>) => {
-  // const [bannerPhoto, setBannerPhoto] = useState<File[]>([]);
 
+  const handleSinglePhotoChange = async(e: ChangeEvent<HTMLInputElement>) => {
+    // const [bannerPhoto, setBannerPhoto] = useState<File[]>([]);
+
+    // if (e.target.files) {
+    //   const selectedPhotos = Array.from(e.target.files);
+    //   setBannerPhoto([...selectedPhotos]);
+    // }
+  
     if (e.target.files) {
-      const selectedPhotos = Array.from(e.target.files);
-      setBannerPhoto([...selectedPhotos]);
+      const formData = new FormData()
+      formData.append("image",e.target.files[0])
+      const res =await fetch(image_hosting_url,{
+        method:"POST",
+        body:formData
+      })
+      const imgResponse = await res.json()
+      if(imgResponse.success){
+        setBannerPhoto(imgResponse.data.display_url)
+      }
     }
   };
 
 
-
-
-
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
-    const values = { ...formData, file: bannerPhoto[0] };
+    const values = { ...formData };
     //@ts-ignore
-    values['age']= parseInt(values['age'])
+    values['age'] = parseInt(values['age'])
+    values['bannerPhoto'] = bannerPhoto
     const data = modifyPayload(values);
-  
+
     const request = await fetch(`${process.env.NEXT_PUBLIC_BECKEN_URL}/pets`, {
       method: "POST",
       headers: {
-        "authorization": localStorage.getItem(`${AuthKey}`) as string,       
+        "authorization": localStorage.getItem(`${AuthKey}`) as string,
       },
       body: data,
     });
-  
+
     const response = await request.json();
-  
+
     if (response.success) {
+      setIsPetDelete(isPetDelete + 1)
       toast.success(response.message);
-      setIsPetDelete(isPetDelete+1)
-          const id= {id :response.data.id}
-          const files =multiplePhotos
-          const values = { ...id, file: files }
-          const data = modifyMultiplePayload(values);   
-          const request= await fetch(`${process.env.NEXT_PUBLIC_BECKEN_URL}/pets/upload-multiple-photos`,{
-            method:"POST",
-            headers: {
-              "authorization": localStorage.getItem(`${AuthKey}`) as string,       
-            },
-            body: data,
-          })       
-          const result =await request.json()
+      const id = { id: response.data.id }
+      const files = multiplePhotos
+      const values = { ...id, file: files }
+      const data = modifyMultiplePayload(values);
+      const request = await fetch(`${process.env.NEXT_PUBLIC_BECKEN_URL}/pets/upload-multiple-photos`, {
+        method: "POST",
+        headers: {
+          "authorization": localStorage.getItem(`${AuthKey}`) as string,
+        },
+        body: data,
+      })
+      const result = await request.json()
 
     } else {
       toast.error('something went wrong');
@@ -163,27 +149,10 @@ const [formData, setFormData] = useState<FormData>(initialFormData);
 
     closeModal();
   };
-  
 
-  // const modifyPayload = () => {
-  //   const payload = new FormData();
 
-  //   // Append form data fields to payload
-  //   for (const [key, value] of Object.entries(formData)) {
-  //     payload.append(key, value as string | Blob);
-  //   }
 
-  //   // Append files to payload
-  //   if (bannerPhoto) {
-  //     payload.append('file', bannerPhoto[0]);
-  //   }
 
-  //   // multiplePhotos.forEach((photo, index) => {
-  //   //   payload.append('multiplePhotos', photo);
-  //   // });
-
-  //   return payload;
-  // };
   const closeModal = () => {
     const myElement = document.getElementById("my_modal_5");
     if (myElement instanceof HTMLDialogElement) {
@@ -207,75 +176,76 @@ const [formData, setFormData] = useState<FormData>(initialFormData);
       <dialog id="my_modal_5" className="modal  ">
         <div className="modal-box min-w-full min-h-screen rounded-none">
           <div className="relative mx-auto px-3  bg-white shadow-md  max-w-5xl ">
-              <button className="text-end absolute top-0 right-0 btn" onClick={closeModal}>
-                close
-              </button>
+            <button className="text-end absolute top-0 right-0 btn" onClick={closeModal}>
+              close
+            </button>
             <h2 className="text-2xl   font-bold mb-3 text-center text-gray-800">
               Add New Pet
             </h2>
 
             <form onSubmit={handleSubmit} method="dialog">
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-              <div className="mb-4">
-                <label className="block text-gray-700">Pet&apos;s Name</label>
-                <input
-                  type="text"
-                  name="name"
-                  value={formData.name}
-                  onChange={handleInputChange}
-                  placeholder="pet's name"
-                  className="mt-1 w-full block p-2 border border-gray-300 rounded-md"
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                <div className="mb-4">
+                  <label className="block text-gray-700">Pet&apos;s Name</label>
+                  <input
+                    type="text"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleInputChange}
+                    placeholder="pet's name"
+                    className="mt-1 w-full block p-2 border border-gray-300 rounded-md"
                   // required
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-700">Pet&apos;s Types</label>
-                <input
-                  type="text"
-                  name="species"
-                  value={formData.species}
-                  onChange={handleInputChange}
-                  placeholder="pet's type"
-                  className="mt-1 w-full block p-2 border border-gray-300 rounded-md"
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700">Pet&apos;s Types</label>
+                  <input
+                    type="text"
+                    name="species"
+                    value={formData.species}
+                    onChange={handleInputChange}
+                    placeholder="pet's type"
+                    className="mt-1 w-full block p-2 border border-gray-300 rounded-md"
                   // required
-                />
-              </div>
+                  />
+                </div>
               </div>
               <div className="mb-4">
                 <label className="block text-gray-700">Banner Photos</label>
                 <input
-                  type="file"                 
+                  type="file"
                   onChange={handleSinglePhotoChange}
                   className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
                   accept="image/*"
-                  // required
+                // required
                 />
-              
-              
 
+
+{bannerPhoto &&
                 <div className="mt-2 flex flex-wrap gap-2">
-                  {bannerPhoto.map((photo, index) => (
+              
                     <Image
                       width={1000}
                       height={1000}
-                      key={index}
-                      src={URL.createObjectURL(photo)}
-                      alt={`Photo ${index + 1}`}
+                    
+                      src={bannerPhoto}
+                      alt={`Photo`}
                       className="h-20 w-20 object-cover rounded-md"
                     />
-                  ))}
-                </div>
+                  
+                </div>}
+
               </div>
               <div className="mb-4">
-                <label className="block text-gray-700">Multiple sample Photos</label>              
-              
+                <label className="block text-gray-700">Multiple sample Photos</label>
+
                 <input
                   type="file"
                   multiple
                   onChange={handlePhotoChange}
                   className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
                   accept="image/*"
-                  // required
+                // required
                 />
 
                 <div className="mt-2 flex flex-wrap gap-2">
@@ -293,7 +263,7 @@ const [formData, setFormData] = useState<FormData>(initialFormData);
               </div>
               <div className="mb-4">
                 <label className="block text-gray-700">
-                 temperament description
+                  temperament description
                 </label>
                 <textarea
                   name="temperament"
@@ -302,7 +272,7 @@ const [formData, setFormData] = useState<FormData>(initialFormData);
                   placeholder="Friendly, energetic, good with other dogs,Max is a playful and affectionate Labrador Retriever looking for a loving home."
                   className="mt-1 block w-full h-[5rem] p-2 border border-gray-300 rounded-md"
                   rows={4}
-                  // required
+                // required
                 ></textarea>
               </div>
               <div className="mb-4">
@@ -316,7 +286,7 @@ const [formData, setFormData] = useState<FormData>(initialFormData);
                   placeholder="detailed description"
                   className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
                   rows={4}
-                  // required
+                // required
                 ></textarea>
               </div>
               <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
@@ -329,7 +299,7 @@ const [formData, setFormData] = useState<FormData>(initialFormData);
                     onChange={handleInputChange}
                     placeholder="age"
                     className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
-                    // required
+                  // required
                   />
                 </div>
                 <div className="mb-4">
@@ -341,7 +311,7 @@ const [formData, setFormData] = useState<FormData>(initialFormData);
                     onChange={handleInputChange}
                     placeholder="breed"
                     className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
-                    // required
+                  // required
                   />
                 </div>
                 <div className="mb-4">
@@ -351,7 +321,7 @@ const [formData, setFormData] = useState<FormData>(initialFormData);
                     value={formData.gender}
                     onChange={handleInputChange}
                     className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
-                    // required
+                  // required
                   >
                     <option value="" disabled>
                       Select Gender
@@ -367,7 +337,7 @@ const [formData, setFormData] = useState<FormData>(initialFormData);
                     value={formData.size}
                     onChange={handleInputChange}
                     className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
-                    // required
+                  // required
                   >
                     <option value="" disabled>
                       Select Size
@@ -386,7 +356,7 @@ const [formData, setFormData] = useState<FormData>(initialFormData);
                     onChange={handleInputChange}
                     placeholder="health status"
                     className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
-                    // required
+                  // required
                   />
                 </div>
                 <div className="mb-4">
@@ -400,7 +370,7 @@ const [formData, setFormData] = useState<FormData>(initialFormData);
                     onChange={handleInputChange}
                     placeholder="current location"
                     className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
-                    // required
+                  // required
                   />
                 </div>
               </div>
